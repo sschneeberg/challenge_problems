4ed3b6467e8484213123e8735297651ad25a6b47
Rome Bell <romebell5@gmail.com>
2020-10-27 05:37:21 -0700

    [1 challenge] Add 2 problems

---
+++ 1_challenge.js
@@ -0,0 +1,32 @@
+/* **************************************************************
+Make a function createNewElement(tagName, text) that takes in 2 
+parameters and returns a new element with the correct text
+************************************************************** */
+function createNewElement(tagName, text) {
+    
+}
+
+console.log(createNewElement('p', 'I am a software engineer'));
+console.log(createNewElement('h1', 'General Assembly'));
+console.log(createNewElement('p', 'SEI 1019'));
+
+/* ************************************************************ */
+
+
+/* **************************************************************
+Make a function giveMeKeys(array) that takes in an array of objects
+and returns a new array with all of the keys from each object
+************************************************************** */
+const instuctors = [
+    { rome: 'Rome', pete: 'Pete'},
+    { mike: 'Mike', dex: 'Dexter'},
+    { ga: 'General Assembly', cohort: 'SEI1019' }
+]
+
+const giveMeKeys = (array) => {
+
+}
+
+console.log(giveMeKeys(instuctors));
+
+/* ************************************************************ */
